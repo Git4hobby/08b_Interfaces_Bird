5c390b908d97a8d361314435e227bc2139bb46f7
Markus <Markus-Clephas@t-online.de>
2025-01-24 12:48:10 +0100

    Implementierung von Interfaces

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,18 @@
+package com.cc.java;
+
+public class Bird implements Flyable, IFeathers{
+
+    @Override
+    public String fly() {
+        return "I can fly at 45mph!";
+    }
+
+    @Override
+    public String hasFeathers() {
+        return "I'm a Bird, I have cleaned nice Feathers!";
+    }
+
+    
+
+
+}
